0c272974992e30c7bc0b591b0102830781c7f0ae
gabriella-silva <gabriellaferreira534@gmail.com>
2025-01-31 14:28:46 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+const searchInput = document.getElementById('search-Input');
+document.addEventListener('input', function(){
+    const searchTerm = searchInput.value.toLowerCase;
+    if (searchTerm === ''){
+        
+    }
+})
+console.log('Its working');
