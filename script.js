d627de364957693a63f357bad16c4e1d7d3e7b10
gabriella-silva <gabriellaferreira534@gmail.com>
2025-01-31 14:40:08 -0300

    .

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const searchInput = document.getElementById('search-Input');
+const resultArtist = document.getElementById('result-artist');
+const resultPlaylist = document.getElementById('result-playlist');
+
+function requestAPI(searchTerm){
+  const url =('http://localhost:3000/artists?name_like=${searchTerm}')
+  fetch(url)
+  .then
+};
+
 document.addEventListener('input', function(){
     const searchTerm = searchInput.value.toLowerCase;
     if (searchTerm === ''){
-        
+        resultPlaylist.classList.add('hidden');
+        resultArtist.classList.remove('hidden');
+        return;
     }
 })
-console.log('Its working');
+console.log('It*s working');
